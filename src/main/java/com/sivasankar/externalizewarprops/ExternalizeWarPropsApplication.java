dffb134df1f3dcbda32e48c781db8aad631e9821
sivasankar.thalavai.dev@gmail.com <sivasankar.thalavai.dev@gmail.com>
2022-04-10 12:11:53 +0530

    sad

--- src/main/java/com/sivasankar/externalizewarprops/ExternalizeWarPropsApplication.java
+++ src/main/java/com/sivasankar/externalizewarprops/ExternalizeWarPropsApplication.java
@@ -1,9 +1,11 @@
 package com.sivasankar.externalizewarprops;
 
+import com.sivasankar.externalizewarprops.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class ExternalizeWarPropsApplication {
 
 	public static void main(String[] args) {
