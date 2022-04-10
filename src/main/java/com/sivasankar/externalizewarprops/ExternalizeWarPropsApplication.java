2ced33b77fb0944b7d42f3e9db94348d878490b4
SivasankarThalavai <60835540+SivasankarThalavai@users.noreply.github.com>
2022-04-10 17:49:11 +0530

    Update ExternalizeWarPropsApplication.java

--- src/main/java/com/sivasankar/externalizewarprops/ExternalizeWarPropsApplication.java
+++ src/main/java/com/sivasankar/externalizewarprops/ExternalizeWarPropsApplication.java
@@ -3,6 +3,7 @@ package com.sivasankar.externalizewarprops;
 import com.sivasankar.externalizewarprops.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
