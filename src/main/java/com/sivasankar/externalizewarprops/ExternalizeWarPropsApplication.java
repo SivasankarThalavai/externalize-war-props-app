7f2e5ea70339f868c57e31efb7929ed9d0350ab6
sivasankar.thalavai.dev@gmail.com <sivasankar.thalavai.dev@gmail.com>
2022-04-10 11:53:09 +0530

    commit1

---
+++ src/main/java/com/sivasankar/externalizewarprops/ExternalizeWarPropsApplication.java
@@ -0,0 +1,13 @@
+package com.sivasankar.externalizewarprops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalizeWarPropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalizeWarPropsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sivasankar/externalizewarprops/ExternalizeWarPropsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivasankar.externalizewarprops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalizeWarPropsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
